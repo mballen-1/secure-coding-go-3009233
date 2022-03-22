7ef9f8a1713223292dbf3acdc81146b892aa6c9c
Miguel Ballén <mballeng91@gmail.com>
2022-03-22 15:36:31 -0500

    ch2: Validate activity struct input for size and consistency

--- Ch02/challenge/activity.go
+++ Ch02/challenge/activity.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"log"
 	"os"
@@ -15,11 +16,39 @@ type Activity struct {
 	Description string    `json:"description"`
 }
 
+func (act *Activity) validate(r io.Reader) error {
+	if len(act.User) == 0 {
+		return fmt.Errorf("missing user")
+	}
+
+	if len(act.Description) == 0 {
+		return fmt.Errorf("missing Description")
+	}
+
+	startTime := act.StartTime
+	endTime := act.EndTime
+
+	if startTime.After(endTime) {
+		return fmt.Errorf("Start time = %s \n should be previous to the end time = %s\n",
+			startTime, endTime)
+	}
+	return nil
+}
+
 func processActivity(r io.Reader) error {
 	var act Activity
 
-	dec := json.NewDecoder(r)
+	const (
+		maxSize = 100 * 1024
+	)
+	dec := json.NewDecoder(io.LimitReader(r, maxSize))
 	if err := dec.Decode(&act); err != nil {
+		return err
+	}
+
+	if err := act.validate(r); err != nil {
+		log.Printf("err: processActivity - %s", err)
+		// http.Error(r, "bad data", http.StatusBadRequest)
 		return err
 	}
 
